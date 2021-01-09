171e5578afff9be9e344a8262a82c6b56f03eda2
amazingProj <68805670+amazingProj@users.noreply.github.com>
2021-01-09 20:33:10 +0200

    Update ex2.c

--- ex5/ex2.c
+++ ex5/ex2.c
@@ -1,182 +1,193 @@
 #include <stdio.h>
 typedef struct ComplexNumber_S
 {
-    float realNumber;
-    float img;
+	float realNumber;
+	float img;
 } ComplexNumber;
 
+/**
+* @brief gets a number
+* @return the input number
+*/
+float GetNumber()
+{
+	float num;
+	while (scanf_s("%f", 9, &num) != 1)
+	{
+		while (getchar() != '\n');
+		printf("Try again: ");
+	}
+	return num;
+}
+
+/**
+*  Getting a char from user
+*  @return a char
+*/
+char GettingOperator()
+{
+	printf("Enter operator: +, -, *, /, any other chars \n");
+	char operatorr;
+	operatorr = getchar();
+	return operatorr;
+}
+
+/**
+*   Getting a string, asks for real or complex 
+*   @return string 
+*/
+char * GettingType()
+{
+	char type[8];
+	printf("Enter number type: put: real or complex!\n");
+	fgets(type, sizeof(type), stdin);
+	return type;
+
+}
+
+/**
+*   Getting a complex or real number
+*   @param - type: real number or complex
+*/
+ComplexNumber GettingComplexNumber(char *type)
+{
+	ComplexNumber result;
+	result.realNumber = 0.0;
+	result.img = 0.0;
+	if (type == "real")
+	{
+		result.realNumber = GetNumber();
+		result.img = 0.0;
+	}
+
+	if (type == "complex")
+	{
+		printf("Enter real number");
+		result.realNumber = GetNumber();
+		printf("Enter image field");
+		result.img = GetNumber();
+
+	}
+	return result;
+}
+
+/**
+*   @param num1 -  a complex number a + bi
+*   @param num2 - a commplex number c + di
+*
+*   @return num1 / num2
+*   formula: (ac + bd) / (c^2 + d^2) + i((bc - ad) / (c^2 + d^2))
+*/
+ComplexNumber Devision(ComplexNumber num1, ComplexNumber num2)
+{
+	ComplexNumber result;
+	float denominator = num2.realNumber * num2.realNumber + num2.img * num2.img;
+	result.realNumber = (num1.realNumber * num2.realNumber + num1.img * num2.img) / 
+		denominator;
+	result.img = (num1.img * num2.realNumber - num1.realNumber * num2.img) /
+		denominator;
+	return result;
+
+}
+
+/**
+*  @param num1 - a + bi
+*  @param num2 - c + di
+*  @return a complex number 
+*  formula:  ac - bd + i(ad + bc)
+*/
+ComplexNumber Multiplicate(ComplexNumber num1, ComplexNumber num2)
+{
+	ComplexNumber result;
+	result.realNumber = num1.realNumber * num2.realNumber - num1.img * num2.img;
+	result.img = num1.realNumber * num2.img + num1.img * num2.realNumber;
+	return result;
+
+}
+
+/**
+*  
+*  @param num1 - a complex number
+*  @param num2 - a complex number
+*
+*  @return num1 - num2 in a complex number
+*/
+ComplexNumber OperatorMinus(ComplexNumber num1, ComplexNumber num2)
+{
+	ComplexNumber result;
+	result.realNumber = num1.realNumber - num2.realNumber;
+	result.img = num1.img - num2.img;
+	return result;
+
+}
+
+/**
+*  adding two complex numbers.
+*  @param num1 - a complex nnumber
+*  @param num2 - a complex number
+*
+*/
+ComplexNumber OperatorPlus(ComplexNumber num1, ComplexNumber num2)
+{
+	ComplexNumber result;
+	result.realNumber = num1.realNumber + num2.realNumber;
+	result.img = num1.img + num2.img;
+	return result;
+
+}
 
 void PrintComplexNumber(ComplexNumber num)
 {
-    if (num.img > 0.0)
-    {
-         printf("%.2f + %.2fi",num.realNumber,num.img);
-    }
-   else
-   {
-        printf("%.2f  %.2fi",num.realNumber,num.img);
-   }
+	if (num.img > 0.0)
+	{
+		printf("%.2f + %.2fi", num.realNumber, num.img);
+	}
+	else
+	{
+		printf("%.2f  %.2fi", num.realNumber, num.img);
+	}
 }
 
 /**
- * @brief gets a number
- * @return the input number
- */
-float GetNumber()
-{
-   float num;
-   while (scanf("%f", &num) != 1)
-   {
-      while (getchar() != '\n');
-      printf ("Try again: ");
-   }
-   return num;
-}
-
-/**
- *         calculator of complex and real numbers
- * @return 0 - when terminates
- */
+*         calculator of complex and real numbers
+* @return 0 - when terminates
+*/
 int main()
 {
-    ComplexNumber cmp3;
-    cmp3.realNumber = 0.0;
-    cmp3.img = 0.0;
-    int number2 = 0;
-    ComplexNumber cmp2;
-    ComplexNumber cmp;
-    int formerIndex;
-    float number;
-    printf("Enter number type: put: real or complex!\n");
-    char* type;
-    scanf("%s", type);
-    if (type == "real")
-    {
-        number = GetNumber();
-        formerIndex = 0;
-    }
-    if (type == "complex")
-    {
-        printf("Enter real number");
-        cmp.realNumber = GetNumber();
-        printf("Enter image field");
-        cmp.img = GetNumber();
-        formerIndex = 1;
+	char *type;
+	type = GettingType();
+	ComplexNumber num1 = GettingComplexNumber(type);
+	
+	char operatorr;
+	int ext = 0;
+	ComplexNumber num2;
+	
+	while (ext == 0)
+	{
+		operatorr = GettingOperator();
+		type = GettingType();
+		num2 = GettingComplexNumber(type);
+		switch (operatorr)
+		{
+		case '+':
+			num1 = OperatorPlus(num1, num2);
+			break;
+		case '*':
+			num1 = Multiplicate(num1, num2);
+			break;
+		case '/':
+			num1 = Devision(num1, num2);
+			break;
+		case '-':
+			num1 = OperatorMinus(num1, num2);
+			break;
+		default:
+			ext = 1;
+			break;
 
-    }
-
-    
-    printf("Enter operator: +, -, *, /, any other chars \n");
-    int ext = 0;
-    char operator;
-    operator = getchar();
-
-    while (ext == 0)
-    {
-        switch (operator)
-        {
-        case '+':
-        printf("Enter number type: put: real or complex!/n");
-        char* type;
-        scanf("%s", type);
-        if (type == "real")
-        {
-            number2 = GetNumber();
-            formerIndex = 0;
-            if (formerIndex == 1)
-            {
-                cmp3.realNumber += cmp.realNumber;
-                cmp3.img += cmp.img;
-                PrintComplexNumber(cmp3);
-            }
-            else
-            {
-                number2 += number;
-                printf("%d", number2);
-            }
-        }
-        
-        if (type == "complex")
-        {
-            printf("Enter real field");
-            cmp2.realNumber = GetNumber();
-            printf("Enter image field");
-            cmp2.img = GetNumber();
-        
-            if (formerIndex == 1)
-            {
-                cmp3.realNumber += cmp2.realNumber;
-                cmp3.img += cmp2.img;
-                PrintComplexNumber(cmp3);
-            }
-            else
-            {
-                cmp2.realNumber += number;
-                PrintComplexNumber(cmp2);
-            }
-        
-
-        }
-
-            break;
-        case '*':
-        printf("Enter number type: put: real or complex!/n");
-        char* type;
-        scanf("%s", type);
-        if (type == "real")
-        {
-            number2 = GetNumber();
-            formerIndex = 0;
-            if (formerIndex == 1)
-            {
-                cmp3.realNumber += cmp.realNumber;
-                cmp3.img += cmp.img;
-                PrintComplexNumber(cmp3);
-            }
-            else
-            {
-                number2 += number;
-                printf("%d", number2);
-            }
-        }
-        
-        if (type == "complex")
-        {
-            printf("Enter real field");
-            cmp2.realNumber = GetNumber();
-            printf("Enter image field");
-            cmp2.img = GetNumber();
-        
-            if (formerIndex == 1)
-            {
-                cmp3.realNumber += cmp2.realNumber;
-                cmp3.img += cmp2.img;
-                PrintComplexNumber(cmp3);
-            }
-            else
-            {
-                cmp2.realNumber += number;
-                PrintComplexNumber(cmp2);
-            }
-        
-
-        }
-            break;
-        case '/':
-            break;
-        case '-':
-            break;
-        default:
-            ext = 1;
-            break;
-
-        }
-    }
-
-
-
-
-
-    printf("Hello, World!\n");
-    return 0;
+		}
+	}
+	
+	PrintComplexNumber(num1);
+	return 0;
 }
