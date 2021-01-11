2f56322982c784e9b20a0711809625866e59428e
amazingProj <68805670+amazingProj@users.noreply.github.com>
2021-01-11 23:50:24 +0200

    Update ex2.c

--- ex5/ex2.c
+++ ex5/ex2.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+
+#define STRING_SIZE 15
+
 typedef struct ComplexNumber_S
 {
 	float realNumber;
@@ -12,7 +17,7 @@ typedef struct ComplexNumber_S
 float GetNumber()
 {
 	float num;
-	while (scanf_s("%f", 9, &num) != 1)
+	while (scanf_s("%f", &num) != 1)
 	{
 		while (getchar() != '\n');
 		printf("Try again: ");
@@ -26,21 +31,22 @@ float GetNumber()
 */
 char GettingOperator()
 {
-	printf("Enter operator: +, -, *, /, any other chars \n");
+	printf("Enter operator: +, -, *, /, (any other chars to leave the program)\nYour choice: ");
 	char operatorr;
 	operatorr = getchar();
+	getchar(); // You need to get the \n so it won't cause trouble when reading another char or string
 	return operatorr;
 }
 
 /**
-*   Getting a string, asks for real or complex 
-*   @return string 
+*   Getting a string, asks for real or complex
+*   @return string
 */
-char * GettingType()
+char* GettingType()
 {
-	char type[8];
-	printf("Enter number type: put: real or complex!\n");
-	fgets(type, sizeof(type), stdin);
+	char* type = (char*)malloc(sizeof(*type) * STRING_SIZE);;
+	printf("Enter number type: put: real or complex!\nYour choice: ");
+	fgets(type, STRING_SIZE, stdin);
 	return type;
 
 }
@@ -49,26 +55,27 @@ char * GettingType()
 *   Getting a complex or real number
 *   @param - type: real number or complex
 */
-ComplexNumber GettingComplexNumber(char *type)
+ComplexNumber GettingComplexNumber(char* type)
 {
 	ComplexNumber result;
 	result.realNumber = 0.0;
 	result.img = 0.0;
-	if (type == "real")
+	if (!strcmp(type, "real\n"))
 	{
-		printf("Enter a number\n");
+		printf("Enter a real number: \n");
 		result.realNumber = GetNumber();
 		result.img = 0.0;
 	}
 
-	if (type == "complex")
+	if (!strcmp(type, "complex\n"))
 	{
-		printf("Enter real number");
+		printf("Enter a real number for the real part: ");
 		result.realNumber = GetNumber();
-		printf("Enter image field");
+		printf("Enter a real number for the imaginary part: ");
 		result.img = GetNumber();
 
 	}
+	getchar(); // since you are reading a char as operator you need to catch the '\n' here also
 	return result;
 }
 
@@ -83,7 +90,7 @@ ComplexNumber Devision(ComplexNumber num1, ComplexNumber num2)
 {
 	ComplexNumber result;
 	float denominator = num2.realNumber * num2.realNumber + num2.img * num2.img;
-	result.realNumber = (num1.realNumber * num2.realNumber + num1.img * num2.img) / 
+	result.realNumber = (num1.realNumber * num2.realNumber + num1.img * num2.img) /
 		denominator;
 	result.img = (num1.img * num2.realNumber - num1.realNumber * num2.img) /
 		denominator;
@@ -94,7 +101,7 @@ ComplexNumber Devision(ComplexNumber num1, ComplexNumber num2)
 /**
 *  @param num1 - a + bi
 *  @param num2 - c + di
-*  @return a complex number 
+*  @return a complex number
 *  formula:  ac - bd + i(ad + bc)
 */
 ComplexNumber Multiplicate(ComplexNumber num1, ComplexNumber num2)
@@ -107,7 +114,7 @@ ComplexNumber Multiplicate(ComplexNumber num1, ComplexNumber num2)
 }
 
 /**
-*  
+*
 *  @param num1 - a complex number
 *  @param num2 - a complex number
 *
@@ -139,13 +146,13 @@ ComplexNumber OperatorPlus(ComplexNumber num1, ComplexNumber num2)
 
 void PrintComplexNumber(ComplexNumber num)
 {
-	if (num.img > 0.0)
+	if (num.img >= 0.0)
 	{
-		printf("%.2f + %.2fi", num.realNumber, num.img);
+		printf("%.2f + %.2fi\n", num.realNumber, num.img);
 	}
 	else
 	{
-		printf("%.2f  %.2fi", num.realNumber, num.img);
+		printf("%.2f - %.2fi\n", num.realNumber, -num.img);
 	}
 }
 
@@ -155,17 +162,21 @@ void PrintComplexNumber(ComplexNumber num)
 */
 int main()
 {
-	char *type;
+	char* type;
 	type = GettingType();
 	ComplexNumber num1 = GettingComplexNumber(type);
-	
+
 	char operatorr;
 	int ext = 0;
 	ComplexNumber num2;
-	
+
 	while (ext == 0)
 	{
 		operatorr = GettingOperator();
+
+		//If the user didn't type one of the operators below then break the loop
+		if (!strchr("+-/*", operatorr))
+			break;
 		type = GettingType();
 		num2 = GettingComplexNumber(type);
 		switch (operatorr)
@@ -182,13 +193,9 @@ int main()
 		case '-':
 			num1 = OperatorMinus(num1, num2);
 			break;
-		default:
-			ext = 1;
-			break;
-
 		}
 	}
-	
+
 	PrintComplexNumber(num1);
 	return 0;
 }
