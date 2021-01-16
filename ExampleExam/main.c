13cc22d7158ab2dc7a7505f137138ccdbe41f389
amazingProj <68805670+amazingProj@users.noreply.github.com>
2021-01-16 23:23:45 +0200

    Update main.c

--- ExampleExam/main.c
+++ ExampleExam/main.c
@@ -18,35 +18,55 @@ void PrintFibonnaciNumber(unsigned long long fib, size_t index)
 int main(void)
 {
 	size_t index = 2;
-	unsigned long long fib = Fibonacci(index - 1);
+	unsigned long long fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 
 	index = 3;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 
 	index = 6;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
-	
+
 	index = 7;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 
 	index = 22;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 
 	index = 29;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 
 	index = 54;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 
 	index = 29;
-	fib = Fibonacci(index - 1);
+	fib = Fibonacci(index);
+	PrintFibonnaciNumber(fib, index);
+
+	index = 60;
+	fib = Fibonacci(index);
 	PrintFibonnaciNumber(fib, index);
 	return 0;
 }
+/*
+ 
+ Example: test's output
+ 
+ Place in series   The number
+               2            1
+               3            2
+               6            8
+               7           13
+              22        17711
+              29       514229
+              54    368225352
+              29       514229
+              60   1820529360
+
+*/
