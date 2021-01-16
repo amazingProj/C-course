55849893d82abb8ec25e9979a608b9b984190c40
amazingProj <68805670+amazingProj@users.noreply.github.com>
2021-01-16 20:56:39 +0200

    Create Fibonaci.h

---
+++ ExampleExam/Fibonaci.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdio.h>
+
+typedef struct DB_t DB;
+
+/**
+ *    calculate the n-th number of fibonnaci series
+ *    @param  n - the number of place
+ *    @return the n-th number.
+ */
+unsigned long long Fibonacci(size_t n);
+
+
